074daaf9553dac0adb98ad7581bb622231edec35
ltaoo <litaowork@aliyun.com>
2018-02-07 11:48:53 +0800

    feat: 支持预览嵌套组件

--- src/common/renderComponent.js
+++ src/common/renderComponent.js
@@ -3,32 +3,43 @@
  * @author wuya
  */
 import React from 'react';
-import {
-    Form
-} from 'antd';
+import { Form } from 'antd';
 
 /**
  * @param {Array} instances - 实例对象数组
  */
 export default function renderComponent(instances, context) {
-    const components = [];
+  if (!instances) {
+    return null;
+  }
+  const components = [];
   for (let i = 0, l = instances.length; i < l; i += 1) {
     const instance = instances[i];
-    const { Component, isField, props, fieldProps, uuid } = instance;
-    
+    const { label, Component, isField, props, fieldProps, uuid, children } = instance;
+
     if (isField) {
-        const { title, label, rules } = fieldProps;
-        const { form } = context;
-        const { getFieldDecorator } = form;
-        components.push(
-            <Form.Item key={uuid} label={title}>
-                {getFieldDecorator(label)(
-                    <Component {...props}></Component>
-                )}
-            </Form.Item>
-        );
+      const { title, label } = fieldProps;
+      const { form } = context;
+      const { getFieldDecorator } = form;
+      components.push(
+        <Form.Item key={uuid} label={title}>
+          {getFieldDecorator(label)(
+            <Component {...props}>{renderComponent(children, context)}</Component>,
+          )}
+        </Form.Item>,
+      );
     } else {
-        components.push(<Component key={uuid} {...props}></Component>);
+      if (label === 'Button') {
+        components.push(
+          <Component key={uuid} {...props}></Component>,
+        );
+      } else {
+        components.push(
+          <Component key={uuid} {...props}>
+            {renderComponent(children, context)}
+          </Component>,
+        );
+      }
     }
   }
   return components;
